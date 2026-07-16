de2e25692c72a4cac42cb45089fa42dc0c5c83a3
deepak porwal <er.deepakporwal@gmail.com>
2026-07-16 20:34:51 +0530

    loadtest added

---
+++ WebApiK6Demo/perfomance/loadtest.js
@@ -0,0 +1,17 @@
+﻿import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+    vus: 10, // Number of virtual users
+    duration: '30s', // Duration of the test
+};
+
+export default function () {
+    let response = http.get('http://localhost:5000/api/values'); // Replace with your API endpoint
+
+    check(response, {
+        'status is 200': (r) => r.status === 200,
+    });
+
+    sleep(1);
+}
