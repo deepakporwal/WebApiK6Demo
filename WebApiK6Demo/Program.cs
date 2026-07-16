98f5b2e2cbdce5915ee2ba652a0107eac202e212
deepak porwal <er.deepakporwal@gmail.com>
2026-07-16 20:19:13 +0530

    Add project files.

---
+++ WebApiK6Demo/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.Map("/", () => Results.Ok("App is running!"));
+
+app.Map("/health", () => Results.Ok("Healthy!"));
+
+app.Map("/hello", () => Results.Ok("Hello from banking api!"));
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
