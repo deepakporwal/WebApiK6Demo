0ef46d499da235cbe57f2a3f6fb4d7a218eeaf28
Deepak Porwal <er.deepakporwal@gmail.com>
2026-07-18 07:48:18 +0530

    Modify load test configuration and endpoint
    Reduced the number of virtual users from 10 to 2 and updated the API endpoint.

--- WebApiK6Demo/perfomance/loadtest.js
+++ WebApiK6Demo/perfomance/loadtest.js
@@ -2,12 +2,12 @@
 import { sleep, check } from 'k6';
 
 export const options = {
-    vus: 10, // Number of virtual users
+    vus: 2, // Number of virtual users
     duration: '30s', // Duration of the test
 };
 
 export default function () {
-    let response = http.get('http://localhost:5000/hello'); // Replace with your API endpoint
+    let response = http.get('https://quickpizza.grafana.com''); // Replace with your API endpoint
 
     check(response, {
         'status is 200': (r) => r.status === 200,
